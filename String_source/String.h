2aff5c3658b99ee3b73a68cbbe533b3a6339178e
Artak Avagyan <92084060+ArtakAvagyan@users.noreply.github.com>
2022-01-24 01:49:50 +0400

    Update String.h

--- String_source/String.h
+++ String_source/String.h
@@ -18,12 +18,12 @@ public:
       String(const String&);
       String(String&&);
       ~String();
+public:
       size_t size()const;
       void Print()const;
       char& at(size_t);
       const char& at(size_t)const;
 public:
-
       char& operator[](size_t);
       const char& operator[](size_t)const;
       String& operator=(const String&);
@@ -36,12 +36,9 @@ public:
       bool operator>=(const String&)const;
       bool operator<=(const String&)const;
 public:
-
-friend std::ostream& operator<<(std::ostream&,const STL::String&);
-//friend std::istream& operator>>(std::istream&,String&);
-
+      friend std::ostream& operator<<(std::ostream&,const STL::String&);
+      //friend std::istream& operator>>(std::istream&,String&);
 private:
-
       size_t m_size;
       char* m_buffer;
 
