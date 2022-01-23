a5fb8b23e4cdf7eafcf11a729933267e400a1889
Artak Avagyan <92084060+ArtakAvagyan@users.noreply.github.com>
2022-01-24 01:43:56 +0400

    Delete tusft.cpp

--- List_source/tusft.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-
-int main()
-{
-//	std::vector<int>vec{1,2,3};
-//	std::cout<< vec.capacity()<<'\n'<<vec[99]<<std::endl;
-	std::cout<< sizeof(void)<<std::endl;
-}
