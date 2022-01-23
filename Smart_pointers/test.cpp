bd04ad8d67183f9dff528c167f0f20287d4f52de
Artak Avagyan <92084060+ArtakAvagyan@users.noreply.github.com>
2022-01-24 01:46:08 +0400

    Update test.cpp

--- Smart_pointers/test.cpp
+++ Smart_pointers/test.cpp
@@ -1,81 +1,8 @@
-/*#include <iostream>
-#include <functional>
-template<typename T,typename del = std::function<void(T*)> >
-class smartPointer {
-public:
-    smartPointer(T* n_ptr = nullptr, del deletor = nullptr)
-    {
-        s_ptr = n_ptr;
-        n_ptr = nullptr;
-        ptr = deletor ;   
-    }
-    smartPointer(smartPointer<T>& other){
-        this->s_ptr = other.s_ptr;
-        other.s_ptr = nullptr;
-        ptr = other.ptr;
-    }
-    smartPointer(smartPointer<T>&& other){
-        this->s_ptr = other.s_ptr;
-        other.s_ptr = nullptr;
-        ptr = other.ptr;
-    }
-    void operator=(smartPointer<T>& other){
-        if(s_ptr!= nullptr)
-            clear();
-        this->s_ptr = other.s_ptr;
-        other.s_ptr = nullptr;
-        ptr = other.ptr;
-    }
-    void operator=(smartPointer<T>&& other){
-        if(s_ptr!= nullptr)
-            clear();
-        this->s_ptr = other.s_ptr;
-        other.s_ptr = nullptr;
-        ptr = other.ptr;
-    }
-    
-    void operator=(T* m_ptr){
-        if(s_ptr!= nullptr)
-            clear();
-        this->s_ptr = m_ptr;
-    }
-    void get_deletor(std::function<void(T*)> deletor)
-    {
-        ptr = deletor;
-    }
-    ~smartPointer(){
-        clear();
-    }
-    T& operator*(){return *s_ptr;}
-    private:
-    void clear()
-    {
-        if(ptr== nullptr){
-            std::cout<<"clap"<<std::endl;
-            delete s_ptr ;
-            s_ptr = nullptr;
-        }
-        else
-        {
-            ptr(s_ptr);
-        }
-    }
-    T* s_ptr;
-    del ptr ;
-};
-int main(){
-    int* b = new int; 
-    smartPointer<int,std::function<void(int*)>> a (b,[&](int*b){std::cout<< "chlp"<<std::endl;});
-    std::cout<< "++++++++++++++++++++++++++++"<<std::endl;
-    *a = 15;
-    std::cout<<*a<<std::endl;
-    std::cout<< "++++++++++++++++++++++++++++"<<std::endl;
-    return 0;
-}*/
-
 #include <functional>
 #include <iostream>
+
 #include "Unique_ptr.h"
+
 int clear(int* ptr)
 {
 	delete[]ptr;
@@ -83,7 +10,7 @@ int clear(int* ptr)
 }
 int main()
 {
-	auto it = [](int*& ptr)->int{delete[]ptr;return 0;};
+	auto it = [] (int*& ptr)->int{delete[]ptr;return 0;};
 	int * tmp = new int[5];
 	STL::Unique_ptr<int,decltype(it)> p(tmp,it);
 	p[0] = 15;
